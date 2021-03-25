760fc81a45c5e8ea9f46783ed9e4853ffee34b5a
guitaric <tarilai94@aol.com>
2021-03-25 11:33:39 +0100

    almost done

--- application/views/webpage.php
+++ application/views/webpage.php
@@ -1,10 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Hello Istvan, this is tough</title>
+	<title>Hello Istvan</title>
 
 	<style type="text/css">
 
@@ -18,8 +20,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		color: black;
 	}
 
-
-
 	h1 {
 		color: #444;
 		font-size: 19px;
@@ -28,19 +28,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		padding: 14px 15px 10px 15px;
 	}
 
-
 	#container {
 		margin: 0 15px 0 15px;
 	}
 
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
 
 	#container {
 		margin: 10px;
@@ -70,28 +61,66 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	
 	.boxes > div > div:first-child {
-		background-color: lightblue;
+		background-color: lightgreen;
 		font-size: 2em;
 		font-weight: bold;
 	}
 
 	.boxes > div > div:nth-child(2) {
-		background-color: blanchedalmond;
+		background-color: white;
+		font-size: 2em;
 	}
 
 	.boxes > div > div:last-child {
-		background-color: lightpink;
+		background-color: red;
 	}
 
 	button {
 		width: 50%;
 		height: 50%;
+		font-weight: bold;
 		border: 5px solid black;
 		background-color: white;
 	} 
 
+	img {
+		width: 400px;
+		height: 300px;
+
+	}
+
+	h1 {
+		display: inline;
+		font-size: 30px;
+		font-weight: bold;
+	}
+
+	.things {
+		display: flex;
+		justify-content: space-evenly ;
+	}
+
+	.things > h1 {
+		line-height: 200px;
+	}
 
 	</style>
+
+	<script>
+
+
+
+// let categ1 = document.getElementById('category1');
+// // categ1.textContent = jokes
+
+
+// function toggleBox() {
+// 	console.log(jokes)
+// }
+	
+</script>
+
+
 </head>
 <body>
 
@@ -99,12 +128,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<h1>Chuck Norris API</h1>
 
     <div id="container">
-		
+
 		<div class='boxes' id='box1'>
 			<div >
 				<div>Chuck Norris Joke</div>
-				<div></div>
-				<div><button>Open for more</button></div>
+				<div class='category'></div>
+				<div><button onclick="toggleBox()">Open for more</button></div>
 			</div>
 			<!-- <div>Chuck Norris is funnier than Zlatan. NOT</div> -->
 		</div> 
@@ -112,7 +141,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	    <div class='boxes' id='box2'>
 			<div>
 				<div>Chuck Norris Joke</div>
-				<div></div>
+				<div class='category'></div>
 				<div><button>Open for more</button></div>
 			</div>
 		</div>
@@ -120,14 +149,55 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<div class='boxes' id='box3'>
 			<div>
 				<div>Chuck Norris Joke</div>
-				<div></div>
+				<div class='category'></div>
 				<div><button>Open for more</button></div>
 			</div>
 		</div> 
 
 	</div>
 
+	<div class='things'>
+		<img src="images/zlatan-pic.jpeg" alt="">
+		<h1>VS.</h1>
+		<img src="images/chuck-norris-pic.jpg" alt="">
+	</div>
+
 </div>
 
 </body>
+
+<script>
+
+var jokes;
+
+fetch('http://api.icndb.com/jokes/random/3').then(function (response) {
+	// The API call was successful!
+	return response.json();
+}).then(function (data) {
+	// This is the JSON from our response
+	jokes = data;
+	console.log(jokes)
+
+	let categories = document.getElementsByClassName('category');
+	console.log(categories[0])
+
+	for(let i=0; i<jokes.value.length; i++) {
+		if(jokes.value[i].categories.length == 0) {
+			categories[i].textContent = "Category: general";
+		} else {
+			categories[i].textContent = "Category: " + jokes.value[i].categories;
+		}
+	}
+
+
+
+}).catch(function (err) {
+	// There was an error
+	console.warn('Something went wrong.', err);
+});
+
+
+
+	</script>
+
 </html>
